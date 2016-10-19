095206edaf13b807ad1fc798bb4782e789f739e6
Daniel Kappelle <daniel.kappelle@gmail.com>
2016-10-19 10:48:01 -0400

    Cleaned up a bit

--- bridge.py
+++ bridge.py
@@ -4,11 +4,11 @@ import requests
 from flask import Flask, request
 from pyxtension.Json import Json
 from jinja2 import Template
+import os.path
 app = Flask(__name__)
 
 webhook_url = "https://platform.etv.tudelft.nl/hooks/"
-template_file = "templates/repo-push"
-template = Template(open(template_file, 'r').read())
+template_folder = "templates/"
 
 
 @app.route("/")
@@ -17,17 +17,17 @@ def hello():
 
 @app.route("/hooks/<hook>",methods=['GET', 'POST'])
 def bla(hook):
-        # event = request.headers.get('X-Event-Key')
-        # if(event == "issue:created"):
-        #     issueCreated(request)
-        # elif(event == "repo:push")
-	data = Json(request.get_json())
-
-	output = template.render(data=data)
-	print(output)
-
-        submitHook("https://platform.etv.tudelft.nl/hooks/%s" % hook, output)
-        return "hoi"
+        event = request.headers.get('X-Event-Key')
+        template_file_name = event.replace(":", "-")
+        template_file = template_folder + template_file_name
+        if(os.path.exists(template_file)):
+            template = Template(open(template_file, 'r').read())
+	    data = Json(request.get_json())
+	    output = template.render(data=data)
+            submitHook("https://platform.etv.tudelft.nl/hooks/%s" % hook, output)
+            return "Done"
+        else:
+            return "Couldn't handle this event"
 
 def submitHook(url, hook_data):
     data = {'text':hook_data}
