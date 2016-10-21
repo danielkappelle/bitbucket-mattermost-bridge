3cbd7623a92c7bf518db7df287aa1f8f0366c0af
Daniel Kappelle <daniel.kappelle@gmail.com>
2016-10-21 04:41:54 -0400

    Did a lot of cleaning up and there is now a separate config file

--- bridge.py
+++ bridge.py
@@ -1,47 +1,77 @@
-import pprint
+########################################
+#   Bitbucket Mattermost Bridge        #
+#                                      #
+#   Written by Daniel Kappelle, 2016   #
+########################################
+
+#import modules
 import json
 import requests
 from flask import Flask, request
 from pyxtension.Json import Json
 from jinja2 import Template
 import os.path
+
+#import user config
+import config
+
+# Initialize flask app
 app = Flask(__name__)
 
-webhook_url = "https://platform.etv.tudelft.nl/hooks/"
+# Folder for the template files
 template_folder = "templates/"
 
 
 @app.route("/")
-def hello():
-        return "Hello World!"
+def renderHomePage():
+    return "<h1>Bitbucket Mattermost Bridge</h1><p>Please refer to the repo on <a href='https://github.com/danielkappelle/bitbucket-mattermost-bridge'>Github</a> for the Readme.</p>"
 
 @app.route("/hooks/<hook>",methods=['GET', 'POST'])
-def bla(hook):
+def bridgeHook(hook):
+        # This function does all the magic
+
+        # The event key is used to determine the type of event
+        # e.g. repo:push, issue:created, etc.
         event = request.headers.get('X-Event-Key')
+
+        # The template folder is searched for a template file
+        # that matches thee event-key, (: replaced by -), e.g.
+        # repo-push
         template_file_name = event.replace(":", "-")
         template_file = template_folder + template_file_name
         if(os.path.exists(template_file)):
-            template = Template(open(template_file, 'r').read())
+            # Parse the json data from the webhook
 	    data = Json(request.get_json())
-            print(template_file)
+
+            # Read the template file and render it using the
+            # data from the webhook
+            template = Template(open(template_file, 'r').read())
 	    output = template.render(data=data)
-            submitHook("https://platform.etv.tudelft.nl/hooks/%s" % hook, output)
+
+            # Submit the new, bridged, webhook to the mattermost
+            # incoming webhook
+            submitHook(config.webhook_url + hook, output)
             return "Done"
         else:
-            return "Couldn't handle this event"
+            # In case there's no templat for the event
+            # throw an error
+            return "Couldn't handle this event", HTTP_500_INTERNAL_SERVER_ERROR
 
 def submitHook(url, hook_data):
+    # This function submits the new hook to mattermost
     data = {'text':hook_data}
     
+    # Post the webhook
     response = requests.post(
             url, data=json.dumps(data),
                 headers={'Content-Type': 'application/json'}
                 )
     if response.status_code != 200:
         raise ValueError(
-                        'Request to slack returned an error %s, the response is:\n%s'
+                        'Request to mattermost returned an error %s, the response is:\n%s'
                         % (response.status_code, response.text)
                         )
 
 if __name__ == "__main__":
-        app.run(host='danielkappelle.com')
+        # Run flask app on host, this is set in config.py
+        app.run(host=config.host, port=config.port)
